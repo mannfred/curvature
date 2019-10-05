543085edb7f139a704f44c0472a65321c1324d7e
mannfred <mannfredboehm@gmail.com>
2019-10-04 17:15:19 -0700

    first plot of size vs curvature (all three species)

---
+++ Rscripts/plotting_size_vs_curv.R
@@ -0,0 +1,56 @@
+library(here)
+library(tidyverse)
+
+#import data ####
+
+#size data
+size_data<-
+  read.csv(
+  here("data/epimedium_curv_size_data.csv"), 
+  header=TRUE)
+
+#curvature data
+
+curv_gran<-
+  read.csv(
+    here("data/epimedium_curvature_grandiflorum.csv"),
+    header=TRUE) %>%
+  select(4, 5) #select columns with species IDs and adjusted curvature data
+  
+curv_kore<-
+  read.csv(
+    here("data/epimedium_curvature_koreanum.csv"),
+    header=TRUE) %>%
+  select(4, 5)
+
+curv_viol<-
+  read.csv(
+    here("data/epimedium_curvature_violaceum.csv"),
+    header=TRUE) %>%
+  select(4, 5)
+
+#merge curvature data into one tibble
+
+curv_data<-
+  full_join(curv_gran, curv_kore) %>%
+  full_join(., curv_viol)
+
+# merge size data with curvature data ####
+
+size_curv_data<-
+  left_join(size_data, curv_data, by="species_individual_panicle_flower") %>%
+  rename(species_ID = species_individual_panicle_flower) %>%
+  mutate(
+    species_epithet = str_extract(
+      species_ID, 
+      "[A-Z]+") #extacts uppercase letters from strings
+  )
+
+#plot
+
+ggplot(
+  data=size_curv_data, 
+  aes(x=sepal_size_mm, y=adjusted_curvature)
+) +
+  geom_point(
+    aes(colour=factor(species_epithet)))
--- Rscripts/plotting_size_vs_days.R
+++ Rscripts/plotting_size_vs_days.R
@@ -1,13 +1,15 @@
 library(here)
 library(tidyverse)
 
-#combine alignment matrix with size data ####
+#for E. grandiflorum ####
+
+#combine alignment matrix with size data 
 
 #alignment data
-stages_days_subset <- 
+stages_days_subset <- #only import E. grandiflorum data
   readRDS("stages_days_sort_grandiflorum.rds") %>%
-  na_if("-") %>%
-  drop_na() %>% #remove rows with "-"
+  na_if("-") %>% 
+  drop_na() %>% #remove rows with "-" 
   group_by(ID) %>% #create groups by flower
   mutate(row_ID = row_number()) %>%
   unite(unique_ID,  ID, row_ID, sep="_") %>% #create unique_ID column
@@ -24,20 +26,23 @@ data5_subset<- data5 %>%
   unite(unique_ID,  Species_Individual_Panicle_Flower, row_ID, sep="_") %>% #create unique_ID column
   ungroup()
 
-joiner<-
+joiner_grandiflorum<-
   stages_days_subset %>%
   left_join( data5_subset, by = "unique_ID")
 
-
+#plot single species
 ggplot(
-  data=joiner, 
-  aes(x=days_elapsed, y=size)
+  data=joiner_grandiflorum, 
+  aes(x=elapsed_days, y=size)
 ) +
-geom_point(
-  aes(colour=factor(species_epithet)))
+  geom_point(
+    aes(x=elapsed_days, y=size))
 
-########### make scatterplot w 3 species
 
+
+# make scatterplot with all 3 species ####
+
+#import elapsed days data
 gran_stages_days <- 
   readRDS("stages_days_sort_grandiflorum.rds") %>%
   na_if("-") %>%
@@ -65,20 +70,19 @@ viol_stages_days <-
   unite(unique_ID,  ID, row_ID, sep="_") %>% #create unique_ID column
   ungroup()
 
-#size data
+#import size data
 data5 <- readRDS("epimedium_growth_data_pivot_redefined_stages.rds")
 
-data5_subset<- data5 %>%
-  # filter(Species_epithet == 'grandiflorum') %>%
-  select(c(1, size)) %>%
+size_data <- data5 %>%
+  select(c(1, size)) %>% #choose columns with species IDs and size data
   group_by(Species_Individual_Panicle_Flower) %>%
-  mutate(row_ID = row_number()) %>%
+  mutate(row_ID = row_number()) %>% #create unique identifiers by row number
   unite(unique_ID,  Species_Individual_Panicle_Flower, row_ID, sep="_") %>% #create unique_ID column
   ungroup()
 
 
-
-joiner<-list(data5_subset, gran_stages_days, kore_stages_days, viol_stages_days) %>%
+#for all three spp.
+joiner<-list(size_data, gran_stages_days, kore_stages_days, viol_stages_days) %>%
   reduce(full_join, by = "unique_ID") %>%
   mutate(days_elapsed = coalesce(elapsed_days.x, elapsed_days.y, elapsed_days)) %>% #create merged days column
   mutate(flower_stage = coalesce(stage.x, stage.y, stage)) %>% #create merged stage column
@@ -86,8 +90,15 @@ joiner<-list(data5_subset, gran_stages_days, kore_stages_days, viol_stages_days)
   mutate(
     species_epithet = str_extract(
       unique_ID, 
-      "[a-z]+") #extacts words from strings
+      "[a-z]+") #extacts lowercase letters from strings
   ) %>%
   group_by(species_epithet)
 
+#plot
+ggplot(
+  data=joiner, 
+  aes(x=days_elapsed, y=size)
+) +
+  geom_point(
+    aes(colour=factor(species_epithet)))
 
