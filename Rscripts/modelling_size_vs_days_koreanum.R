cd5b3e7f7dadf966b769d25b988501d29601482f
mannfred <mannfredboehm@gmail.com>
2019-10-07 09:19:40 -0700

    predicting elapsed days for E. koreanum and E. violaceum curvature datasets

--- Rscripts/modelling_size_vs_days_koreanum.R
+++ Rscripts/modelling_size_vs_days_koreanum.R
@@ -3,13 +3,13 @@ library(tidyverse)
 library(drc) #for logistic models 
 
 #logistic models
-#for E. grandiflorum ####
+#for E. koreanum ####
 
 #combine alignment matrix (elapsed days) with size data 
 
 #import alignment data
-stages_days_gran <- #import E. grandiflorum data
-  readRDS(here("data/RDS_files/stages_days_sort_grandiflorum.rds")) %>%
+stages_days_kore <- #import E. koreanum data
+  readRDS(here("data/RDS_files/stages_days_sort_koreanum.rds")) %>%
   na_if("-") %>% 
   drop_na() %>% #remove rows with "-" 
   group_by(ID) %>% #create groups by flower
@@ -20,8 +20,8 @@ stages_days_gran <- #import E. grandiflorum data
 #import size data
 data5 <- readRDS(here("data/RDS_files/epimedium_growth_data_pivot_redefined_stages.rds"))
 
-data5_gran <- data5 %>%
-  filter(Species_epithet == 'grandiflorum') %>%
+data5_kore <- data5 %>%
+  filter(Species_epithet == 'koreanum') %>%
   dplyr::select(c(1, size)) %>% #select masked by drc/aomisc packages 
   group_by(Species_Individual_Panicle_Flower) %>%
   mutate(row_ID = row_number()) %>%
@@ -29,44 +29,52 @@ data5_gran <- data5 %>%
   ungroup()
 
 #join alignment and size data
-joiner_grandiflorum<-
-  stages_days_gran %>%
-  left_join( data5_gran, by = "unique_ID")
+joiner_koreanum<-
+  stages_days_kore %>%
+  left_join( data5_kore, by = "unique_ID")
 
 
 #plotting elapsed_days vs size 
-ggplot(joiner_grandiflorum, aes(x=elapsed_days, y=size)) + 
+ggplot(joiner_koreanum, aes(x=elapsed_days, y=size)) + 
   geom_point(alpha=.5) +
-  ylab("size (mm)") + xlim(0,21)
-
-
+  ylab("size (mm)") + xlim(0,27)
 
 
 #################################################################
 # using model to predict elapsed days from curvature dataset ####
 
-model_gran <-
-  drm(size ~ elapsed_days, fct = L.4(), data=joiner_grandiflorum)
+model_kore <-
+  drm(size ~ elapsed_days, fct = L.4(), data=joiner_koreanum)
 
-plot(model_gran, log="", main = "Logistic function")
+plot(model_kore, log="", main = "Logistic function")
 
 #import size data used to make curvature measurements
-size_data_gran<-
+size_data_kore<-
   read.csv(
     here("data/epimedium_curv_size_data.csv"), 
     header=TRUE) %>%
-  slice(1:19) #select grandiflorum only
+  slice(20:50) #select koreanum only
 
 #use model to predict "elapsed days" from size data
-size_data_gran$predicted_days_elapsed<-
-  predict(model_gran, data.frame(size_data_gran$sepal_size_mm))
+size_data_kore$predicted_days_elapsed<-
+  predict(model_kore, data.frame(size_data_kore$sepal_size_mm))
 
-saveRDS(size_data_gran, file=here("data/RDS_files/size_data_grandiflorum.rds"))
+saveRDS(size_data_kore, file=here("data/RDS_files/size_data_koreanum.rds"))
 
-ggplot(size_data_gran, aes(x=sepal_size_mm, y=predicted_days_elapsed)) + 
+#plot 
+ggplot(size_data_kore, aes(x=sepal_size_mm, y=predicted_days_elapsed)) + 
   geom_point(alpha=.5) +
   ylab("predicted_elapsed_days") + xlim(0,35)
 
 
+###########################
+#append curvature data ####
+
+#import curvature data
+curv_kore<-
+  read.csv(
+    here("data/epimedium_curvature_koreanum.csv"),
+    header=TRUE) %>%
+  dplyr::select(4, 5)
 
 
