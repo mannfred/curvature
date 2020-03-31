40b78f7f2368c7cc9cdb084f518820c630a553bb
mannfred <mannfredboehm@gmail.com>
2020-03-31 11:21:12 -0700

    trajectory analysis

--- Rscripts/3_geomorph_analysis.R
+++ Rscripts/3_geomorph_analysis.R
@@ -2,22 +2,6 @@ library(tidyverse)
 library(here)
 library(geomorph)
 
-#https://qualityandinnovation.com/2019/09/26/why-anova-and-linear-regression-are-the-same/
-#http://www.emmasherratt.com/uploads/2/1/6/0/21606686/quick_guide_to_geomorph-_introduction.html
-#https://www.cambridge.org/core/journals/paleobiology/article/many-faces-of-synapsid-cranial-allometry/D4DF84985CA3222FBE327AD993394E3D/core-reader
-
-
-#read separately 
-kor_data <- 
-  readland.tps(
-    here("data/epimedium_photos/koreanum/koreanum_dorsal_appended_geomorph.TPS"), 
-    specID="imageID")
-
-vio_data <- 
-  readland.tps(
-    here("data/epimedium_photos/violaceum/violaceum_dorsal_appended_geomorph.TPS"), 
-    specID="imageID")
-
 
 #read together
 epi_data <- 
@@ -27,88 +11,97 @@ epi_data <-
   specID="imageID"
 )
 
-#colours
-colour_ids <-
-  c(replicate(31, "#3e8dba"), #31 E. koreanum samples
-    replicate(27, "#663593")) #27 E. violaceum samples
+#import size class data
+#rename and bin developmental stages
+curv_data <- 
+  read.csv(here("data/epimedium_curv_size_data.csv")) %>% 
+  rename(sample = species_individual_panicle_flower) %>% 
+  slice(20:77) %>%  #keep E. kor and E. vio
+  mutate(new_stage = case_when(
+    stage == "E" ~ "C",
+    stage == "G" ~ "G",
+    stage == "ND" | stage == "F" | stage == "N" | stage == "O" ~ "T",
+    stage == "P" | stage == "D" | stage == "A" ~ "A"))
+
+#boxplot
+ggplot(
+  data=curv_data %>% 
+    filter(species=="koreanum" | species=="violaceum"), #remove filter to include grandiflorum
+  aes(x=new_stage, y=sepal_size_mm)) +
+geom_boxplot(
+  aes(fill = factor(species), #colour by species
+      factor(new_stage, levels=c("C", "G", "T", "A")) #reorder
+    )
+  ) +
+  theme(axis.text.x = element_text(angle=90)) +
+  theme_classic() + #removes gray backdrop
+  theme(legend.position="bottom")  
+
+
 
 #groups
 group_ids <-
   c(replicate(31, "E. koreanum"),
-    replicate(27, "E. violaceum"))
+    replicate(27, "E. violaceum")) %>% 
+  factor()
+
 
 #Procrustes Alignment
-proc_data <- gpagen(epi_data) 
+proc_data <- 
+  gpagen(epi_data) 
 
-#basic PCA
-pca_data <- plotTangentSpace(proc_data$coords, groups=colour_ids, label=TRUE)
+
+#plot tangent space
+pca_data <- 
+  plotTangentSpace(proc_data$coords, groups=colour_ids, label=TRUE)
+
 
 #build geomorph dataframe
 gdf <- 
   geomorph.data.frame(
-    proc_data, 
-    PC1 = pca_data$pc.scores[,1], 
-    PC2 = pca_data$pc.scores[,2], 
-    PC3 = pca_data$pc.scores[,3],
-    groups = group_ids, 
-    colors=colour_ids)
+    coords = two.d.array(proc_data$coords), 
+    groups = group_ids,
+    colors = colour_ids,
+    new_stage = factor(curv_data$new_stage),
+    Csize = proc_data$Csize)
 
 
-  
+#is shape determined by developmental stage and taxon?
+fit1 <- lm.rrpp(coords ~ new_stage*groups, data = gdf, iter = 199)
 
-fit1 <- procD.lm(coords ~ log(Csize)*groups, data=gdf, iter=999, RRPP=FALSE)
+
+#trajectory analysis
+TA1 <- 
+  trajectory.analysis(
+    fit1, 
+    groups = gdf$groups, 
+    traj.pts = gdf$new_stage, 
+    print.progress = FALSE)
+
+
+#plot groups*stage morphospace
+TP1 <- 
+  plot(TA1, pch = as.numeric(gdf$groups) + 20, 
+       bg = as.numeric(gdf$new_stage),
+       cex = 1.5, col = "gray")
+
+#add trajectory lines per species
+add.trajectories(TP1, traj.pch = c(21,22), start.bg = 1, end.bg = 2)
+
+#add legend
+legend("topright", levels(gdf$groups), pch =  c(21, 22), pt.bg = 1)
+
+
+# allometry
+fit2 <- procD.lm(coords ~ Csize*groups, data = gdf, iter = 199)
+
 plotAllometry(
-  fit1, 
+  fit2, 
   size=gdf$Csize, 
   logsz = TRUE, 
   method="PredLine",
-  col=colour_ids,
+  col=as.numeric(gdf$groups),
   cex=1.5,
-  pch=16,
-  alpha = I(1/2))
+  pch=16)
 
-
-
-
-#PC variances
-pca_data$pc.summary
-
-
-#Test whether species differ in ontogenetic trajectory: All Species
-AllTaxa.OntogenyComp <- procD.allometry(coords ~ log(Csize), ~species, logsz = TRUE, data=gdf, iter = 9999)
-
-
-
-curv_data <- read.csv(here("data/epimedium_curv_size_data.csv")) %>% 
-             rename(sample = species_individual_panicle_flower) %>% 
-             filter(grepl('K', sample)) #filter for E. koreanum
-
-proc_data$size <- curv_data$sepal_size_mm
-
-fit <- lm.rrpp(coords ~ Csize, data=proc_data, iter=199)
-
-data(plethodon)
-plethodon$site
-
-gpagen(lmk_data) %>%
-  procD.lm()
-
-
-
-
-
-
-
-
-#exponential growth
-y <- c(1,2,4,5,7,8,9,10,11,13,14,20,24,27,30,34,47,54,60,67,78,96,117,157,196,250,324,425,569,690,846,1055,1281,1429,2061,2541)
-x <- 1:36
-data<-data.frame(x,y)
-mod <- nls(y ~ exp(a + b * x), data = data, start = list(a = 1, b = 1))
-mod
-
-plot(data)
-lines(data$x, predict(mod))
-
-f <- function(x) exp(-0.2571 + 0.2246*x)
-#f(40) = 6166 cases predicted for March 28
+legend("topright", levels(gdf$groups), col=c(1,2), pch=16, pt.bg = 1)
